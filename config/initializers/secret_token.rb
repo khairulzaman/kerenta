495bd8634551ba027316a6782850564fdd41fa41
Khairulzaman Kamarulzaman <khairulzaman.kamarulzaman@gmail.com>
2012-01-22 20:01:28 +0800

    Basic structure for project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kerenta::Application.config.secret_token = 'b9f9046b0a37da60e6db3069c571970c069fab2f0903ade9d34660c2428ba5b684a4ae91216cb1a99c18199bf4463ed586b20a9a6d1344f5ebb712ddfde7cb94'
